c91d8812619c37ade83fc9487adc13fdd4116ef2
Le Thanh <nmlethanh91@gmail.com>
2016-04-12 23:08:12 +0200

    Implement half of the 30 opcodes

--- rust/src/chip8.rs
+++ rust/src/chip8.rs
@@ -10,5 +10,228 @@ struct Chip8 {
     stack       : [u16; 16],
     sp          : u16,
     key         : [u8; 16],
+
+    fontset     : [u8; 80],
 }
 
+impl Chip8 {
+    fn initialize(&self) {
+        self.indexReg = 0;
+        self.progCount = 0;
+        self.sp = 0;
+
+        for i in self.memory.iter(){
+            i = 0;
+        }
+        for i in 0usize..16 {
+            self.regV[i] = 0;
+            self.stack[i] = 0;
+            self.key[i] = 0;
+        }
+        self.fontset =
+        [
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        ];
+        
+
+    }
+    fn emulateCycle(&self) {
+        self.opcode = self.memory[self.pc] << 8 | self.memory[self.pc + 1];
+        match self.opcode & 0xF000 {
+            0x0000 => self.opcode0xxx(), 
+            0x1000 => self.opcode1xxx(), 
+            0x2000 => self.opcode0xxx(), 
+            0x3000 => self.opcode0xxx(), 
+            0x4000 => println!("Calls subroutine at NNN."),
+            0x5000 => println!("Sets I to the address NNN."),
+            0x6000 => println!("Jumps to the address NNN plus V0."),
+            0x7000 => self.opcode0xxx(), 
+            0x8000 => println!("Jumps to address NNN."),
+            0x9000 => println!("Calls subroutine at NNN."),
+            0xA000 => println!("N."),
+            0xB000 => println!("Calls subroutine at NNN."),
+            0xC000 => println!("Sets I to the address NNN."),
+            0xD000 => println!("Jumps to the address NNN plus V0."),
+            0xE000 => println!("Jumps to the address NNN plus V0."),
+            0xF000 => println!("Jumps to the address NNN plus V0."),
+
+            _ => println!(""),
+        }
+    }
+    fn opcode0xxx(&self) {
+        match self.opcode & 0x000F{
+            0x0000 => for i in self.gfx.iter() {
+                        i = 0;
+                      },
+            0x000E => {
+                        self.pc = self.stack[self.sp as usize];
+                        self.sp -= 1;
+                      },
+            _      => println!("Unknown opcode"),
+        }
+    }
+    fn opcode1xxx(&self) {
+        self.pc = self.opcode & 0x0FFF
+    }
+    fn opcode2xxx(&self) {
+        self.stack[self.sp as usize] = self.pc;
+        self.sp += 1;
+        pc = self.opcode & 0x0FFF;
+    }
+    fn opcode3xxx(&self) {
+        let index :usize = (self.opcode & 0x0F00) >> 8;
+        let addrt = self.opcode & 0x00FF;
+        self.pc += 2;
+        if regV[index] == addrt {
+            self.pc += 2;
+        }
+       
+    }
+    fn opcode4xxx(&self) {
+        let index :usize = (self.opcode & 0x0F00) >> 8;
+        let addrt = self.opcode & 0x00FF;
+        self.pc += 2;
+        if regV[index] != addrt {
+            self.pc += 2;
+        }
+    }
+    fn opcode5xxx(&self) {
+        let indexX :usize = (self.opcode & 0x0F00) >> 8;
+        let indexY :usize = (self.opcode & 0x00F0) >> 4;
+        self.pc += 2;
+        if self.regV[indexX] == self.regV[indexY] {
+            self.pc += 2;
+        }
+    }
+
+    fn opcode6xxx(&self) {
+        let indexX :usize = (self.opcode & 0x0F00) >> 8;
+        self.regV[indexX] = self.opecode & 0x00FF;
+        self.pc += 2;
+    }
+
+    fn opcode7xxx(&self) {
+        let indexX :usize = (self.opcode & 0x0F00) >> 8;
+        self.regV[indexX] += self.opecode & 0x00FF;
+        self.pc += 2;
+    }
+    fn opcode8xxx(&self) {
+        
+        let indexX :usize = (self.opcode & 0x0F00) >> 8;
+        let indexY :usize = (self.opcode & 0x00F0) >> 4;
+        self.pc += 2;
+        
+        match self.opcode & 0x000F {
+            0x0000 => self.regV[indexX] = self.regV[indexY],
+            0x0001 => self.regV[indexX] = self.regV[indexX] | self.regV[indexY],
+            0x0002 => self.regV[indexX] = self.regV[indexX] & self.regV[indexY],
+            0x0003 => self.regV[indexX] = self.regV[indexX] ^ self.regV[indexY],
+            0x0004 => {
+                        if self.regV[indexX] + self.regV[indexY] > 0xFF {
+                            self.regV[0xF as usize] = 1;
+                        }
+                        else {
+                            self.regV[0xF as usize] = 0;
+                        }
+                        self.regV[indexX] += self.regV[indexY];
+                      },
+            0x0005 => {
+                        if self.regV[indexX] - self.regV[indexY] < 0x00 {
+                            self.regV[0xF as usize] = 0;
+                        }
+                        else {
+                            self.regV[0xF as usize] = 1;
+                        }
+                        self.regV[indexX] += self.regV[indexY];
+                      },
+            0x0006 => {
+                        self.regV[0xF as usize] = self.regV[indexX] & 1;
+                        self.regV[indexX] = self.regV[indexX] >> 1;
+                      },
+            0x0007 => {
+                        if self.regV[indexY] - self.regV[indexX] < 0x00 {
+                            self.regV[0xF as usize] = 0;
+                        }
+                        else {
+                            self.regV[0xF as usize] = 1;
+                        }
+                        self.regV[indexX] = self.regV[indexY] - self.regV[indexX];
+                      },
+            0x000E => {
+                        self.regV[0xF as usize] = self.regV[indexX] >> 15;
+                        self.regV[indexX] = self.regV[indexX] << 1;
+                      },
+        }
+    }
+    fn opcode9xxx(&self) {
+        let indexX :usize = (self.opcode & 0x0F00) >> 8;
+        let indexY :usize = (self.opcode & 0x00F0) >> 4;
+        self.pc += 2;
+        if self.regV[indexX] != self.regV[indexY] {
+            self.pc += 2;
+        }
+
+    }
+    fn opcodeAxxx(&self) {
+        match self.opcode & 0x000F{
+            0x0000 => println!("Clear screen"),
+            0x000E => println!("Return from sub routine"),
+            _      => println!("Unknown opcode"),
+        }
+    }
+    fn opcodeBxxx(&self) {
+        match self.opcode & 0x000F{
+            0x0000 => println!("Clear screen"),
+            0x000E => println!("Return from sub routine"),
+            _      => println!("Unknown opcode"),
+        }
+    }
+    fn opcodeCxxx(&self) {
+        match self.opcode & 0x000F{
+            0x0000 => println!("Clear screen"),
+            0x000E => println!("Return from sub routine"),
+            _      => println!("Unknown opcode"),
+        }
+    }
+    fn opcodeDxxx(&self) {
+        match self.opcode & 0x000F{
+            0x0000 => println!("Clear screen"),
+            0x000E => println!("Return from sub routine"),
+            _      => println!("Unknown opcode"),
+        }
+    }
+    fn opcodeExxx(&self) {
+        match self.opcode & 0x000F{
+            0x0000 => println!("Clear screen"),
+            0x000E => println!("Return from sub routine"),
+            _      => println!("Unknown opcode"),
+        }
+    }
+    fn opcodeFxxx(&self) {
+        match self.opcode & 0x000F{
+            0x0000 => println!("Clear screen"),
+            0x000E => println!("Return from sub routine"),
+            _      => println!("Unknown opcode"),
+        }
+    }
+    fn loadProgram(&self, buffer : &mut Vec<u8>) {
+       for i in 0usize..buffer.len() {
+           self.memory[i + 512] = buffer[i];
+       }
+    }
+}
